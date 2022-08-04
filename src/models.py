bac43a76bab14c190f2bfcf890db84d14ff16c52
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-08-04 00:24:16 +0000

    avance

--- src/models.py
+++ src/models.py
@@ -1,4 +1,12 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
 from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
