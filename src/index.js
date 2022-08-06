8e8c37da81fe7f18dcf25b3325ac7d6a09917536
matiasmunozs <chmunozs@gmail.com>
2022-08-06 01:55:53 +0000

    cambios

--- src/index.js
+++ src/index.js
@@ -4,25 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@popperjs/core';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './index.css';
-import Navbar from './Navbar';
-import PasswordRecoveryEmail from './components/passwordrecoveryemail';
-import PerfilUsuario from './components/PerfilUsuario';
-import PerfilCasino from './components/PerfilCasino';
-import PerfilEmpresa from './components/PerfilEmpresa';
-import AdminEmpresa from './components/AdminEmpresa';
-import ResumenPanelAdmin from './components/ResumenPanelAdmin';
-import DireccionesUsuario from './components/DireccionesUsuario';
 
 
+import Home from './components/Home/Home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
-
-
-   
+     <Home />
   </React.StrictMode>
 );
-
