f031664d95c78f00170a20abacf9096c9051006b
MaRuCien <100109406+MaRuCien@users.noreply.github.com>
2022-08-05 21:03:14 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@popperjs/core';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './index.css';
-import Navbar from './components/Navbar';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
+
   </React.StrictMode>
 );
 
