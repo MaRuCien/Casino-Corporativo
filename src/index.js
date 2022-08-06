8ba9fe979a21348f76ab5f9c637e2e44775fdd0a
MaRuCien <100109406+MaRuCien@users.noreply.github.com>
2022-08-05 21:44:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 import './index.css';
 
 
+import Home from './components/Home/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+     <Home />
   </React.StrictMode>
 );
 
